ae8fb29469f000d5d71f8022fcd2b05a431d7dac
JLP2000 <josh15phan@gmail.com>
2023-07-11 14:31:22 +0100

    fix: added description argument for variable 'file_contents'

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 
 # input variable
 variable "file_contents" {
-  type    = string
-  default = "hello world"
+  type        = string
+  description = "An example input variable used by the null resource and local file"
+  default     = "hello world"
 }
